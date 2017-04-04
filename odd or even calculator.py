e49d459f1f650bd93fd53c7f6c6a65279910de38
Danhyal <Danhyal@users.noreply.github.com>
2017-04-04 14:12:24 +0100

    odd or even

---
+++ odd or even calculator.py
@@ -0,0 +1,18 @@
+import math
+import random
+import sys
+import os
+import time
+
+
+
+number = int(input("enter a number to find out if its odd or even:"))
+
+
+if number % 2 == 0:
+ print ("Even!")
+
+else:
+    print ("ODD!")
+
+
